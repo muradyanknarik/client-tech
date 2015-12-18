ad3efffae2802b7845fb35d80d9509e36918db59
sharptek <pik926535>
2015-12-18 14:21:33 +0200

    Fix

--- phpinfo.php
+++ phpinfo.php
@@ -1,4 +1,15 @@
 <?php
-echo "localhost/phpinfo.php";
+$a = array(2,4,6,8);
+$mins = array();
+$mins[] = $a[0];
+for($i = 1; $i < count($a); $i++){	
+	if( abs($a[$i] - 5) == abs($mins[0] - 5) ){
+		$mins[] = $a[$i];
+	}else if( abs($a[$i] - 5) < abs($mins[0] - 5) ){
+		$mins = array();
+		$mins[] = $a[$i];
+	}
+}
+
 phpinfo();
 ?>
